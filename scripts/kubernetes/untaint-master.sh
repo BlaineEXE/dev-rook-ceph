c6f6d454280ad6ec1f04aab91fe90909a61270a8
Blaine Gardner <blaine.gardner@suse.com>
2019-05-10 18:17:09 -0600

    Fix k8s untaint master script
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- scripts/kubernetes/untaint-master.sh
+++ scripts/kubernetes/untaint-master.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo -n "Untainting master node(s) ... "
+
 noschedule_nodes="$(kubectl get nodes -o json | \
-  jq -r '.items[] | select(.spec.taints[]?.key=="node-role.kubernetes.io/master" and .spec.taints[]?.effect=="NoSchedule") | .metadata.labels."kubernetes.io/hostname"' )"
+  jq -r '.items[] | select(.spec.taints[]?.key=="node-role.kubernetes.io/master") | .metadata.labels."kubernetes.io/hostname"' )"
 
 for node in $noschedule_nodes; do
-  kubectl taint node "${node}" node-role.kubernetes.io/master-
+  suppress_output_unless_error "kubectl taint node ${node} node-role.kubernetes.io/master-"
 done
+
+echo "done."
