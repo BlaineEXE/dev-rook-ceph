2ca2de0f93e9aa595f414f4088656af804a8fbda
Blaine Gardner <blaine.gardner@suse.com>
2019-07-12 14:35:56 -0600

    Change upgrade targets for v1.0 -> master/v1.1
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- scripts/upgrade/rook.sh
+++ scripts/upgrade/rook.sh
@@ -10,15 +10,21 @@ if [[ -s "${UPGRADE_TO_CONFIG_BRANCH_FILE}" ]]; then
 fi
 echo "Upgrading Rook from config in branch '${from_branch}' to config in ${to_branch} ... "
 
-for delete_me in ${UPGRADE_TO_CONFIG_DIR}/ceph/upgrade-*-delete.yaml; do
-  kubectl delete -f "${delete_me}"
-done
+# for delete_me in ${UPGRADE_TO_CONFIG_DIR}/ceph/upgrade-*-delete.yaml; do
+#   set -x
+#   kubectl delete -f "${delete_me}"
+#   set +x
+# done
 
 for create_me in ${UPGRADE_TO_CONFIG_DIR}/ceph/upgrade-*-create.yaml; do
+  set -x
   kubectl apply -f "${create_me}"
+  set +x
 done
 
+set -x
 kubectl --namespace "${ROOK_SYSTEM_NAMESPACE}" set image deploy/rook-ceph-operator \
   rook-ceph-operator=rook/ceph:master
+set +x
 
 echo "  ... done. The upgrade is commencing."
