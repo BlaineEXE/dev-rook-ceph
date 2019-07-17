54bf2d6ad93a49bf50cce50ae92fd3c045f3dc0e
Blaine Gardner <blaine.gardner@suse.com>
2019-07-17 12:25:40 -0600

    Don't change psps for integration tests
    Rook now supports running integrations in psp environments
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- scripts/rook/install.sh
+++ scripts/rook/install.sh
@@ -10,19 +10,23 @@ root_dir="${PWD}"
 echo ''
 echo 'INSTALLING CEPH'
 ( cd "${ROOK_CONFIG_DIR}"/ceph
-  if [[ ! -s psp.yaml ]]; then
-    default_psp="${root_dir}/scripts/rook/default-psp.yaml"
-    echo "  WARNING! The current config branch lacks a 'psp.yaml' file."
-    echo "           Using the dev-rook-ceph default '${default_psp}'."
-    cp "${default_psp}" psp.yaml
-  fi
+  # if [[ ! -s psp.yaml ]]; then
+  #   default_psp="${root_dir}/scripts/rook/default-psp.yaml"
+  #   echo "  WARNING! The current config branch lacks a 'psp.yaml' file."
+  #   echo "           Using the dev-rook-ceph default '${default_psp}'."
+  #   cp "${default_psp}" psp.yaml
+  # fi
   if [[ -s common.yaml ]]; then
-    kubectl apply -f common.yaml -f psp.yaml -f operator.yaml
-  else
-    # this will be able to install Rook v0.9 manifests
-    kubectl apply -f psp.yaml -f operator.yaml
+    # does not exist for v0.9
+    kubectl apply -f common.yaml
   fi
-  sleep 3 # I think it takes a while for the CRDs to create. Workaround by sleeping a few seconds.
+  if [[ -s psp.yaml ]]; then
+    # does not exist after psp added to common.yaml
+    kubectl apply -f psp.yaml
+  fi
+  kubectl apply -f operator.yaml
+
+  sleep 10 # I think it takes a while for the CRDs to create. Workaround by sleeping a few seconds.
   kubectl apply -f cluster.yaml -f toolbox.yaml
   sleep 3
   kubectl apply -f storageclass.yaml # allows block storage
