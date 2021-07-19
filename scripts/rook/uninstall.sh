7e4dde2a7d771cf8b58f44b3b546c379c0682d64
Blaine Gardner <blaine.gardner@redhat.com>
2021-07-19 13:13:36 -0600

    change uninstall to just remove all manifests
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>

--- scripts/rook/uninstall.sh
+++ scripts/rook/uninstall.sh
@@ -9,19 +9,19 @@ trap '' ERR
 echo ''
 echo 'DELETING ROOK RESOURCES'
 
-# FIRST delete the Ceph cluster; if we don't do this and just try to delte all the resources, the
-# next install may then detect a delete event at the end and then remove the cluster we want to test
-cluster="$(kubectl --namespace ${ROOK_NAMESPACE} get CephCluster --output name)"
-kubectl --namespace ${ROOK_NAMESPACE} delete "${cluster}" --wait=false
+# # FIRST delete the Ceph cluster; if we don't do this and just try to delte all the resources, the
+# # next install may then detect a delete event at the end and then remove the cluster we want to test
+# cluster="$(kubectl --namespace ${ROOK_NAMESPACE} get CephCluster --output name)"
+# kubectl --namespace ${ROOK_NAMESPACE} delete "${cluster}" --wait=false
 
-wait_for "CephCluster to start Terminating pods" 25 \
-  "kubectl --namespace ${ROOK_NAMESPACE} get pod --no-headers | grep -q rook-ceph-mgr | grep Terminating"
+# wait_for "CephCluster to start Terminating pods" 25 \
+#   "kubectl --namespace ${ROOK_NAMESPACE} get pod --no-headers | grep -q rook-ceph-mgr | grep Terminating"
 
-# Wait for cluster pods to be done running; mgr is one of last to be deleted usually
-wait_for "Rook resources to be deleted" 200 \
-  "! kubectl --namespace ${ROOK_NAMESPACE} get pod --no-headers | grep -q rook-ceph-mgr"
+# # Wait for cluster pods to be done running; mgr is one of last to be deleted usually
+# wait_for "Rook resources to be deleted" 200 \
+#   "! kubectl --namespace ${ROOK_NAMESPACE} get pod --no-headers | grep -q rook-ceph-mgr"
 
-# NEXT go through all the yaml files and do deletes on them to catch any other stuff
+# go through all the yaml files and do deletes on them
 ( cd "${ROOK_CONFIG_DIR}"/ceph
   del_args=""
   for f in $(find . -name '*.yaml'); do
